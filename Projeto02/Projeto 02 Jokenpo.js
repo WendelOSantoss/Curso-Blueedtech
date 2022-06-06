882a31a7a2f991da7c2d66037b25f54079504e4d
Wendel Santos <106357851+WendelOSantoss@users.noreply.github.com>
2022-06-06 19:09:21 -0300

    Projeto02 Jokenpo Definitivo

--- Projeto02/Projeto 02 Jokenpo.js
+++ Projeto02/Projeto 02 Jokenpo.js
@@ -1,6 +1,6 @@
 const prompt = require("prompt-sync")();
+while(true){
 console.clear();
-
 let nome = prompt("Qual o seu nome?");
 console.log(`Olá ${nome},hoje iremos jogar jokenpo,conhecido também como pedra papel e tesoura`);
 console.log("As regras são simples. Papel ganha de Pedra e perde pra tesoura");
@@ -12,15 +12,16 @@ let rodadas = prompt("Quantas rodadas deseja jogar?");
 let escolha = ["pedra","papel","tesoura"];
 let pontuacaojogador= 0;
 let pontuacaocomputador = 0;
-let empate=0;
 for(i=0;i<rodadas;i++){
     let jogador = prompt("Escolha entre pedra, papel e tesoura")
+    while (jogador != "pedra" && jogador!="papel" && jogador != "tesoura"){
+        jogador= prompt("Você só pode escolher entre pedra, papel e tesoura, por favor digite corretamente");
+    }
     let computador = escolha[Math.floor(Math.random()*escolha.length)]
     console.log (jogador)
     console.log (computador)
     if(jogador=="pedra" && computador=="pedra"){
         console.log ("Empate")
-        empate++
     }
     else if(jogador=="pedra" && computador=="tesoura"){
         console.log("Vitória do jogador")
@@ -32,7 +33,6 @@ for(i=0;i<rodadas;i++){
     }
         if(jogador=="tesoura" && computador=="tesoura"){
         console.log ("Empate")
-        empate++
     }
         else if(jogador=="tesoura" && computador=="papel"){
         console.log("Vitória do jogador")
@@ -42,9 +42,8 @@ for(i=0;i<rodadas;i++){
         console.log("Vitória do computador")
         pontuacaocomputador++
     }
-             if(jogador=="papel" && computador=="papel"){
+            if(jogador=="papel" && computador=="papel"){
             console.log ("Empate")
-            empate++
         }
             else if(jogador=="papel" && computador=="pedra"){
             console.log("Vitória do jogador")
@@ -55,17 +54,31 @@ for(i=0;i<rodadas;i++){
             pontuacaocomputador++
         }          
     }
-        if(pontuacaojogador>pontuacaocomputador && pontuacaojogador>empate){
-            console.log(`O grande vencedor foi ${nome}, meus parabéns, suas habilidades são incríveis!`)
+        console.log(`A pontuação do jogador foi de ${pontuacaojogador}`);
+        console.log(`A pontuação do computador foi de ${pontuacaocomputador}`);
+
+        if(pontuacaojogador>pontuacaocomputador){
+            console.log(`O grande vencedor foi ${nome}, meus parabéns, suas habilidades são incríveis!`);
         }
-            else if(pontuacaocomputador>pontuacaojogador && pontuacaocomputador>empate){
-                console.log(`O grande vencedor foi o computador, lamentável perder pra uma máquina ${nome}, tente outra vez`)
+            else if(pontuacaocomputador>pontuacaojogador){
+                console.log(`O grande vencedor foi o computador, lamentável perder pra uma máquina ${nome}, tente outra vez`);
             }
-                else if(empate>pontuacaojogador && empate>pontuacaocomputador){
-                    console.log("O resultado foi empate, vocês se mostraram adversários dignos")
+                else(console.log("O resultado foi empate, vocês se mostraram adversários dignos"));
+       
+        let repetir =prompt("Grande partida! Você deseja jogar novamente? Responda com sim ou nao")
+            while(repetir!= "sim" && repetir != "nao"){
+                repetir=prompt("Apenas escolha entre sim ou nao");
+            }
+            if(repetir=="sim"){
+                continue
+            }
+                else{
+                    console.log("Tudo bem, te esperamos na próxima, valeu!");
+                    console.clear();
+                    break
                 }
-
-
+         }
+                    
 
 
 
