ccfabfa7ab693a0fffb397c4edb2614474cc813b
Wendel Santos <106357851+WendelOSantoss@users.noreply.github.com>
2022-06-10 16:52:08 -0300

    Add files via upload

--- Projeto03/Jogo da velha.js
+++ Projeto03/Jogo da velha.js
@@ -7,14 +7,35 @@ let jogodavelha = [
   [0,0,0],
 ];
 
+let jogada = "x"
 
 let linha= +prompt("Digite a linha que quer fazer sua jogada")
 let coluna = +prompt("Digite a coluna que quer fazer sua jogada")
 
+const condicaodevitoria=[
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
 
-let x = 2
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
 
-jogodavelha[linha][coluna]= x
+  [0,4,8],
+  [2,4,6],
+]
+
+for(const condicaodevitoria of condicaodevitoria){
+  for(const coordenada of condicaodevitoria){
+    let linha = parseint(coordenada/3);
+    let coluna = coordenada % 3;
+
+  }
+}
+
+
+
+jogodavelha[linha][coluna]= jogada
 
 
 
