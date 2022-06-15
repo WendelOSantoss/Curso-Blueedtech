8ce53d8d3e509e3e7dcf0441b1517f70bcf4779c
Wendel Santos <106357851+WendelOSantoss@users.noreply.github.com>
2022-06-14 22:53:19 -0300

    Add files via upload

---
+++ Projeto04/Ficçãointerativa.js
@@ -0,0 +1,16 @@
+const prompt = require('prompt-sync')();
+console.clear();
+console.log("Olá Mikonos, você acordou após tirarem dos seus braços sua amada princesa, ela foi aprisionada numa torre pelo temido Bowser, aqui começa sua jornada para resgatá-la, preparado?");
+prompt("Aperte Enter para continuar");
+console.log("Este será um jogo interativo, cada decisão que tomar será essencial para chegar ou não ao seu objetivo, tome sábias decisões, vamos lá?");
+prompt("Aperte Enter para continuar");
+
+Personagem: Mikonos
+Desafios:  Salvar a princesa da torre
+Olá Mikonos, você acordou após tirarem dos seus braços sua amada princesa, ela foi aprisionada numa torre pelo temido Bowser, aqui começa sua jornada para resgata-la, preparado?
+Este será um jogo interativo, cada decisão que tomar será essencial para chegar ou não ao seu objetivo, tome sábias decisões
+
+
+
+
+
