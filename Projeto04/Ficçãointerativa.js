86395f73bba6e51e2e9b4e9dcd53ebbfe25f04d2
Wendel Santos <106357851+WendelOSantoss@users.noreply.github.com>
2022-06-20 16:10:54 -0300

    Update Ficçãointerativa.js

--- Projeto04/Ficçãointerativa.js
+++ Projeto04/Ficçãointerativa.js
@@ -5,18 +5,18 @@ console.clear();
 
 console.log("Olá Mikonos, você acordou após tirarem dos seus braços sua amada princesa, ela foi aprisionada numa torre pelo temido Bowser, aqui começa sua jornada para resgatá-la, preparado?");
 prompt("Aperte Enter para continuar");
-console.log("Este será um jogo interativo, cada decisão que tomar será essencial para chegar ou não ao seu objetivo, tome sábias decisões, vamos lá?");
+console.log("Este será um jogo interativo, cada decisão que tomar será essencial para chegar ou não ao seu objetivo, tome sábias decisões,você tem o tempo de 24 horas no jogo para salva-la, vamos lá?");
 prompt("Aperte Enter para continuar");
 let heroi = {
     nome:"Mikonos",
-    sono:50,
-    fome:50,
-    saude:50,
+    tempo: 24,
+    sono: 50,
+    alertasoldados: 0,
+    saude: 50,
     armas:['Espada', 'Arco e fecha', 'Escudo'],
-    comer: function () {
-        this.fome = false;
-        this.mantimentos--;
-    },
+    saude: function () {
+        this.saude--;
+    },   
 }
 let dias = 0;
 let princesarecuperada=false;
@@ -87,45 +87,140 @@ if (escolha1 == "1"){
             while(escolha5 != "1" && escolha5 != "2"){
             escolha5 = +prompt("Escolha entre opção 1 ou 2");
             }
-        if (escolha5 = "1"){
+        if (escolha5 == "1"){
             console.log(`Você volta para a casa da filha do padeiro e aguarda a poeira abaixar para voltar ao posto avançado durante a noite.`);
+            prompt(`Aperte Enter para continuar`);
+            console.log(`Anoiteceu, você retorna ao posto avançado, você percebe um reforço de soldados devido à sua visita mais cedo ao posto avançado, precisa decidir:
+            1. Adentrar mesmo com os reforços de guarda, tentando ao máximo não ser visto.
+            2. Esperar mais um pouco para ver se os guardas vão se dispersar.`);
+            let escolha6 = +prompt("Escolha entre opção 1 ou 2");
+            while(escolha6 != "1" && escolha6 != "2"){
+            escolha6 = +prompt("Escolha entre opção 1 ou 2");
+            }
+            if(escolha6=="1"){
+                console.log(`Você adentra o posto avançado, porém ao chegar no escritório do capitão dos capangas percebe dois guardas parados em frente a porta`)
+            }
+            else if(escolha6 == "2"){
+                console.log(`Sua espera dá resultado e percebe que os guardas se dispersaram, consegue chegar à sala do capitão dos capangas e começa a vasculhar em busca de pistas`);
+                prompt("Aperte Enter para continuar");
+                console.log(`Depois de um bom tempo vasculhando, você finalmente encontra uma pista de onde a princesa está presa`)
+            }
+        
         }
-        if (escolha5 = "2"){
+        else if (escolha5 = "2"){
             console.log(`Você trava uma intensa batalha com o guarda, porém por se tratar de um soldado iniciante você consegue abate-lo facilmente usando sua espada. E segue rumo ao escritório de um dos capitães dos capangas de bowser.`);
             prompt ("Aperte Enter pra continuar");
-            console.log(`Depois de vasculhar todo o local você encontra uma carta que indica a localização da princesa.`);
+            console.log(`Depois de vasculhar todo o local você encontra uma carta que indica a localização da princesa. Por ter entrado durante o dia você sai a noite do posto avançado com a informação do esconderijo da princesa em mãos, e parte rumo à torre onde estão aprisionando-a.`);
 
         }
     }
 }
+
+
 if (escolha1 == "2"){
 
     console.log(`Seguindo as pegadas você consegue ver de longe dois soldados acampando e precisa se decidir:
     1. Ir conversar em busca de informações
     2. Ataca-los furtivamente`);
 
-    let escolha3 = +prompt("Escolha entre opção 1 ou 2");
-    while(escolha1 != "1" && escolha1 != "2"){
-        escolha3 = +prompt("Escolha entre opção 1 ou 2");
+    let escolha7 = +prompt("Escolha entre opção 1 ou 2");
+    while(escolha7 != "1" && escolha7 != "2"){
+        escolha7 = +prompt("Escolha entre opção 1 ou 2");
     }   
-    if(escolha3 == "1"){
-        console.log (`Os campangas de bowser o reconhecem e te atacam fazendo você receber bastante dano, porém ainda está de pé é necessário tomar uma decisão:
+    if(escolha7 == "1"){
+        console.log (`Os capangas de bowser o reconhecem e te atacam fazendo você receber bastante dano, porém ainda está de pé é necessário tomar uma decisão:
         1.Continuar lutando e tentar derrotar os dois soldados
         2.Tentar fugir`)
 
-        let escolha4 = +prompt("Escolha entre opção 1 ou 2");
-    while(escolha1 != "1" && escolha1 != "2"){
-        escolha4 = +prompt("Escolha entre opção 1 ou 2");
+        let escolha8 = +prompt("Escolha entre opção 1 ou 2");
+        while(escolha8 != "1" && escolha8 != "2"){
+        escolha8 = +prompt("Escolha entre opção 1 ou 2");
         }
         
-    if(escolha4 == "1"){
-        console.log ("Você foi derrotado pelos dois soldados, sua saga termina aqui.");
-    }
-    else if (escolha4 == "2"){
-        console.log ("Você consegue fugir de volta à filha do padeiro que trata dos seus ferimentos, porém isso custa horas preciosas da jornada.");
+        if(escolha8 == "1"){
+        console.log (`Você não resiste aos ferimentos e foi derrotado pelos dois soldados, sua saga termina aqui.`);
+            break
+         }
+        else if (escolha8 == "2"){
+        console.log (`Você consegue fugir de volta à filha do padeiro que trata dos seus ferimentos, porém isso custa horas preciosas da jornada.`);
         prompt("Aperte enter para continuar");
+        console.log(`Apesar de não estar totalmente recuperado, você se sente apto a continuar sua jornada, e se dirige até o local da última batalha. Ao chegar você deve novamente tomar a decisão:
+        1.Seguir as pegadas
+        2.Seguir o rastro de sangue`);
+        let escolha9 = +prompt("Escolha entre opção 1 ou 2");
+        while(escolha9 != "1" && escolha9 != "2"){
+        escolha9 = +prompt("Escolha entre opção 1 ou 2");
+        }
+        if(escolha9 =="1"){
+            console.log(`Você segue as pegadas imaginando encontrar os soldados novamente, porém ao chegar você ve que não tem mais ninguém, foi apenas uma perda de tempo.`);
+            prompt("Aperte Enter para continuar");
+            console.log('Infelizmente você falhou na sua missão, sua jornada termina aqui.');
+                break
+        }   
+
+        else if( escolha9 =="2"){
+                console.log(`Ao seguir o rastro de sangue você encontra um corpo e começa a vasculha-lo, lá indica um posto avançado dos capangas de bowser. Finalmente uma pista válida.`);
+                prompt("Pressione Enter para continuar");
+                console.log(`Ao chegar no posto avançado você se vê com duas opções:
+                1. Esperar anoitecer para entrar mais facilmente, correndo o risco de perder tempo precioso
+                2. Entrar durante o dia esgueirando-se do modo mais furtivo possível, mas com maior risco de ser visto`);
+                let escolha10 = +prompt("Escolha entre opção 1 ou 2");
+                while(escolha10 != "1" && escolha10 != "2"){
+                    escolha10 = +prompt("Escolha entre opção 1 ou 2");
+                }
+            
+                if (escolha10 == "1"){
+                    console.log(`Você consegue adentrar o posto avançado com certa facilidade, e segue rumo ao escritório de um capitão dos capangas de bowser.`);
+                    prompt ("Aperte Enter pra continuar");
+                    console.log(`Depois de muita procura consegue encontrar uma carta que indica onde a princesa está sendo aprisionada. Já está amanhecendo e você precisa fazer uma escolha.
+                    1.Sair mesmo com o dia claro em sentido à torre onde está sendo mantida a princesa, correndo o risco de ser visto.
+                    2.Optar por esperar anoitecer e poder sair do posto avançado com maior segurança. Porém perdendo um bom tempo no processo.`);
+                    
+                    let escolha11 = +prompt("Escolha entre opção 1 ou 2");
+                    while(escolha11 != "1" && escolha11 != "2"){
+                    escolha12 = +prompt("Escolha entre opção 1 ou 2");
+                    }
+                    if (escolha11=="1"){
+                        console.log(`Quando está prestes a sair da base você é visto por um guarda e precisa tomar uma difícil decisão
+                        1.Continuar fugindo, já que você possui a informação que tanto procurava, não se importando com o guarda
+                        2.Enfrentar o guarda para que ele não alerte os companheiros`);
+                        let escolha12 = +prompt("Escolha entre opção 1 ou 2");
+                        while(escolha12 != "1" && escolha12 != "2"){
+                        escolha13 = +prompt("Escolha entre opção 1 ou 2");
+                        if (escolha12 = "1"){
+                            console.log("Depois de uma longa jornada você chegou à torre onde tudo indica estar a princesa. Porém por ter ignorado o guarda encontra vários soldados alertados por ele que estavam à sua espera e acaba morto encerrando sua jornada sem conseguir salvar sua amada princesa.")
+                        }
+                        else if (escolha12 = "2"){
+                            console.log(`Você trava uma intensa batalha com o guarda, porém por se tratar de um soldado iniciante você consegue abate-lo facilmente usando sua espada. E segue rumo à torre onde está a princesa.`);
+                        }
+            
+                        }
+                        
+                    }
+                }
+            
+                    else if (escolha11=="2"){
+                        console.log(`Anoiteceu, você consegue sair do posto avançado sem maiores problemas, e segue rumo à torre onde está a princesa.`);
+                    }
+                }
+
+        }
+        
+        }
     }
 
+    else if(escolha7=="2"){
+        console.log(`Você os ataca furtivamente e consegue eliminar um dos soldados facilmente, porém o segundo soldado percebe a sua presença e uma batalha se inicia`);
+        prompt("Pressione Enter para continuar");
+        console.log(`Usando sua espada e escudo após tomar muito dano, num golpe de misericórdia consegue superar seu adversário.`);
+        prompt("Pressione Enter para continuar");
+        console.log(`Após analisar o acampamento, você percebe um baú junto aos soldados e resolve vasculha-lo. Nele tem um caderno com todas as informações da missão de raptar a princesa, inclusive o local para qual ela foi levada.`);
+        prompt("Pressione Enter para continuar");
+        console.log(`Você segue rumo à torre, para resgata-la o mais rápido possível`) ;  
     }
+
     
-}
+
+
+
+
